d375c1c35f6218cc5a7bf4c277c282c5f3d44ef4
reganzhang <reganzhang@podoteng.com>
2024-03-27 15:09:56 +0800

    [add]所有权和引用

--- 所有权及引用/src/bin/reference.rs
+++ 所有权及引用/src/bin/reference.rs
@@ -6,6 +6,7 @@ fn main() {
     // 不会转移所有权
     let ref_name = &name;
     let ref_nick_name = &name;
+
     println!("ref_name:{ref_name} ref_nick_name:{ref_nick_name}");
     // 打印出引用的地址和值的地址
     println!("ref_name_p:{:p} name_p:{:p}", &ref_name, &(*ref_name));
@@ -22,13 +23,21 @@ fn main() {
         name: String::from("兽兽"),
     };
     println!("ruster name:{}", ruster.get_name());
+
+
+
+    // 可变引用
+    let mut age = 123;
+    let age1 = &mut age;
+    // 使用*解引用，并对其值进行加法操作
+    *age1 += 10;
+    println!("age1:{age1}");
 }
 
 // 函数接收引用参数引用
 fn do_somthing(msg: &String) {
     println!("msg:{msg}");
 }
-
 
 struct Ruster {
     name: String,

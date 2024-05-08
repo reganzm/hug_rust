9cb8303592727e00dd2c3e5aaacd59f09d82fd3d
reganzhang <reganzhang@podoteng.com>
2024-05-08 18:34:29 +0800

    [add]解释器模式

---
+++ 设计模式/interpreter/src/bin/interpreter.rs
@@ -0,0 +1,42 @@
+//! 解释器模式
+trait Expression {
+    fn interpret(&self) -> i32;
+}
+
+// 数字表达式
+struct NumberExpression {
+    value: i32,
+}
+impl NumberExpression {
+    fn new(value: i32) -> Self {
+        NumberExpression { value }
+    }
+}
+impl Expression for NumberExpression {
+    fn interpret(&self) -> i32 {
+        self.value
+    }
+}
+// 加法表达式
+struct AddExpression {
+    left: Box<dyn Expression>,
+    right: Box<dyn Expression>,
+}
+impl AddExpression {
+    fn new(left: Box<dyn Expression>, right: Box<dyn Expression>) -> Self {
+        Self { left, right }
+    }
+}
+impl Expression for AddExpression {
+    fn interpret(&self) -> i32 {
+        self.left.interpret() + self.right.interpret()
+    }
+}
+
+fn main() {
+    let first = NumberExpression::new(10);
+    let second = NumberExpression::new(99);
+    let add = AddExpression::new(Box::new(first), Box::new(second));
+    let result = add.interpret();
+    println!("result:{result}");
+}

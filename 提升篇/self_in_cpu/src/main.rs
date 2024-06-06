77cbc911334ecbaef28efc1999121376de62368c
reganzhang <reganzhang@podoteng.com>
2024-06-06 16:11:47 +0800

    [add]cpu眼里的self

--- 提升篇/self_in_cpu/src/main.rs
+++ 提升篇/self_in_cpu/src/main.rs
@@ -15,3 +15,22 @@ pub fn main() {
     let d = get(b);
     println!("b:{:p} d:{:p}", b, d);
 }
+
+// example::get::h64f063fea484ae4e:
+//         mov     rax, rdi
+//         ret
+
+// example::A::get::h6be26c2822996cdb:
+//         mov     rax, rdi
+//         ret
+
+// example::main::h238d10f39e0a48fc:
+//         push    rax
+//         lea     rdi, [rip + .L__unnamed_1]
+//         call    qword ptr [rip + example::A::get::h6be26c2822996cdb@GOTPCREL]
+//         mov     rdi, rax
+//         call    qword ptr [rip + example::get::h64f063fea484ae4e@GOTPCREL]
+//         pop     rax
+//         ret
+
+// .L__unnamed_1:

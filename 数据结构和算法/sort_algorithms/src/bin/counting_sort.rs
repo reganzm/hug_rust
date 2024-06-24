681da60e07e7decf87de0c0943b7d83537b0b9f9
reganzhang <reganzhang@podoteng.com>
2024-06-24 16:30:53 +0800

    [add]基数排序

--- 数据结构和算法/sort_algorithms/src/bin/counting_sort.rs
+++ 数据结构和算法/sort_algorithms/src/bin/counting_sort.rs
@@ -1,7 +1,6 @@
 //! 计数排序
 //! 平均时间复杂度O(n+k) 空间复杂度O(n+k) 稳定 n为待排数组长度，k为待排数据个数
 
-
 fn counting_sort(nums: &mut [usize]) {
     if nums.len() <= 1 {
         return;
@@ -33,7 +32,7 @@ fn counting_sort(nums: &mut [usize]) {
 }
 
 fn main() {
-    let mut nums = [88999, 99, 66, 33, 123, 45, 890, 10, 18,897543];
+    let mut nums = [88999, 99, 66, 33, 123, 45, 890, 10, 18, 897543];
     println!("原始值：{:?}", &nums);
     counting_sort(&mut nums);
     println!("排序结果:{:?}", nums);
